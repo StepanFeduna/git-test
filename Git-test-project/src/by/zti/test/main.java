7ccbc218d4bbf7b7e3cd9ed85b9f3abc58d42442
Stepan <Stepan@10.44.6.7>
2016-08-19 12:51:28 +0300

    add 1 !

--- Git-test-project/src/by/zti/test/main.java
+++ Git-test-project/src/by/zti/test/main.java
@@ -2,7 +2,7 @@ package by.zti.test;
 
 public class main {
 	public static void main(String[] args){
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!!");
 	}
 
 }
